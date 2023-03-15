229a4937c60ce6c176db66ec0e9df9f270379e96
jimmy <jarech948@gmail.com>
2023-03-15 14:50:34 -0300

    Initial Commit

---
+++ main.py
@@ -0,0 +1,37 @@
+import tkinter
+from tkinter import filedialog as fd
+
+
+def select_directory():
+    # select directory when button pressed
+    directory_text = fd.askdirectory()
+    directoryLineText.set(directory_text)
+
+
+# create main GUI window
+window = tkinter.Tk()
+window.title(" Directory Organizer")
+
+# label for directory input line
+directoryLabel = tkinter.Label(window, text="Directory: ")
+directoryLabel.grid(row=0)
+
+# create entry line for manual or automatic directory path
+directoryLineText = tkinter.StringVar()
+directoryLine = tkinter.Entry(window, width=36, textvariable=directoryLineText)
+directoryLine.grid(row=0, column=1, columnspan=2)
+
+# button activates automatic directory path output from OS File Explorer
+directoryButton = tkinter.Button(window, width=8, text="...", command=select_directory)
+directoryButton.grid(row=0, column=3)
+
+# button to start organization of directory
+startButton = tkinter.Button(window, text="Organize Directory", width=18)
+startButton.grid(row=1, column=0)
+
+# button to close window
+closeButton = tkinter.Button(window, text="Close", width=18, command=window.destroy)
+closeButton.grid(row=1, column=3)
+
+# Main GUI loop
+window.mainloop()
